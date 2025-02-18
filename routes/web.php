069e0f103b8ebcc76406b37cf37dbac2d5ae81a3
Ashfaq-Foysol <ashfaqfoysol007@gmail.com>
2025-02-18 21:10:18 +0600

    fronted complete class 2

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyCommerceController;
-
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 
 Route::get('/', [MyCommerceController::class, 'index'])->name('home');
-Route::get('/category', [MyCommerceController::class, 'category'])->name('category');
-Route::get('/detail', [MyCommerceController::class, 'detail'])->name('detail');
+Route::get('/product-category', [MyCommerceController::class, 'category'])->name('product-category');
+Route::get('/product-detail', [MyCommerceController::class, 'detail'])->name('product-detail');
+Route::get('/show-cart', [CartController::class, 'index'])->name('show-cart');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
